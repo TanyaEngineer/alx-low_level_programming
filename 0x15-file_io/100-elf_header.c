b9845b8f50c2661ae74b88d9f3bf46565788ccbc
TanyaEngineer <tanyaradzwamaribini@gmail.com>
2023-10-02 22:01:05 +0200

    second correction on 100 elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,11 +1,15 @@
+#include <stdio.h>
 #include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
 /**
-  * placeholder2 - define function
-  * @void: describe argument
-  * Return: 0 on success
-  */
-int placeholder2(void)
+ * main - Copies the content of a file to another file.
+ * @ac: Argument count
+ * @av: argument values
+ * Return: 0 on succes, -1 on error.
+ */
+int main(int ac, char *av[])
 {
-	return (0);
+return (1);
+return (0);
 }
-
